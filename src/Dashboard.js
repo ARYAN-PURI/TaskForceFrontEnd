c295c0e3f65208bbb9eb8d385fd3704b57e44301
ARYAN-PURI <140064234+ARYAN-PURI@users.noreply.github.com>
2024-08-16 23:29:38 +0530

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,6 +10,12 @@ export default function Dashboard(){
         let data=await fetch('https://taskforcebackend.onrender.com/getcards');
         let response=await data.json();
         console.log(response);
+        for (let i = 0; i < response.length / 2; i++) {
+            let rand = Math.floor(Math.random() * response.length);
+            let temp = response[rand];
+            response[rand] = response[i];
+            response[i] = temp;
+        }
         setCardData(response);
     }
     React.useEffect(()=>{
